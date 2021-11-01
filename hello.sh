710ff70a7f24ec74a05071e0dcdc1683b484db2b
nagaraju <nagaraj7495@gmail.com>
2021-11-01 09:03:14 +0000

    new line in child

--- hello.sh
+++ hello.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+echo "new line in child branch"
+
 echo "hello world.commiting from the child branch and we are going to commit tem and pushing them to the child of remote repository"
