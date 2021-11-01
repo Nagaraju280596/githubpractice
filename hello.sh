6656fe50c331614781dbc11cf5b5ef1a90849df5
nagaraju <nagaraj7495@gmail.com>
2021-11-01 08:50:12 +0000

    child to child

--- hello.sh
+++ hello.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo "hello world.commiting from the child branch"
+echo "hello world.commiting from the child branch and we are going to commit tem and pushing them to the child of remote repository"
