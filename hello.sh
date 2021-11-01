675550dfa224c92363e2753f7c080d3c52eb511f
nagaraju <nagaraj7495@gmail.com>
2021-11-01 09:29:40 +0000

    second merging

--- hello.sh
+++ hello.sh
@@ -4,4 +4,6 @@ echo "new line in child branch"
 
 echo "he is the merging concept exist"
 
+echo "here is the second merging concept practice"
+
 echo "hello world.commiting from the child branch and we are going to commit tem and pushing them to the child of remote repository"
